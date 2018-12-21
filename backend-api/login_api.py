0f37583f12abc00e6e5336916dbb0585ebe3af7e
董晓 <846373685@qq.com>
2018-12-21 17:54:06 +0800

    push

--- backend-api/login_api.py
+++ backend-api/login_api.py
@@ -8,17 +8,9 @@ def Login():
     resp = make_response(jsonify({'error': False}))
     # 跨域设置
     resp.headers['Access-Control-Allow-Origin'] = '*'
-    print(url_for('login'))
+    print(url_for('Login',name='hello'))
     return resp
 
-@app.route('/user/<name>')
-def user(name):
-    pass
-
-@app.route('/test')
-def test():
-    print(url_for('user', name='letian'))
-    return 'Hello'
 
 if __name__ == '__main__':
     app.run()
